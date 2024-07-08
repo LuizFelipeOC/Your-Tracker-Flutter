d873e49c566d415d95db5400cf40d6f53076132c
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-07-08 11:33:11 -0400

    feat(config): scaffold mensseger key

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:your_tracker/app/pages/search_packages.dart';
 
-void main() => runApp(const AppWidget());
-
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
 
   @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  GlobalKey<ScaffoldMessengerState> scaffoldState = GlobalKey<ScaffoldMessengerState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      scaffoldMessengerKey: scaffoldState,
       debugShowCheckedModeBanner: false,
       title: 'Your Tracker',
+      home: const SearchPackgesPage(),
     );
   }
 }
