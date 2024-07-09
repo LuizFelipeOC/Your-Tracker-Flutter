dca43c6fe2c8f995f717628e60d79123dcf6db16
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-07-09 10:12:27 -0400

    chore(dp): config dp SearchPackageController

--- lib/app/shared/providers.dart
+++ lib/app/shared/providers.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:uno/uno.dart';
+import 'package:your_tracker/app/controller/packages/search_package_controller.dart';
+import 'package:your_tracker/app/data/package/search_package/search_package_repository.dart';
 import 'package:your_tracker/app/services/client/client.dart';
 import 'package:your_tracker/app/services/client/uno_client.dart';
 import 'package:your_tracker/app/shared/app_url.dart';
@@ -12,5 +14,10 @@ void setup() {
 
   getIt.registerLazySingleton<IClient>(
     () => UnoClient(uno: getIt.get<Uno>(), appUrl: getIt.get<AppUrl>()),
+  );
+
+  getIt.registerSingleton(SearchPackageRepository(client: getIt.get<IClient>()));
+  getIt.registerSingleton(
+    SearchPackageController(searchPackageRepository: getIt.get<SearchPackageRepository>()),
   );
 }
